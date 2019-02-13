eae7ca438c1acc8bfbe5e5dc8c2ae41d09356a41
Orestes Polyzos <orestes.polyzos@gmail.com>
2019-02-14 01:20:14 +0200

    Adds Rabbit targets for Archery Game

---
+++ Assets/ArcheryGame/Rabbits/CustomRabbits/Scripts/RabbitRun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitRun : MonoBehaviour
+{
+
+    private string[] m_buttonNames = new string[] { "Idle", "Run", "Dead" };
+
+    private Animator m_animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        // starts running
+        m_animator.SetTrigger("Next");
+        m_animator.SetInteger("AnimIndex", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //If this tag is evilrabbit add points, if this tag is good remove points
+        m_animator.SetTrigger("Next");
+        m_animator.SetInteger("AnimIndex", 3);
+        Destroy(gameObject, 2);
+    }
+}
---
+++ Assets/ArcheryGame/Rabbits/CustomRabbits/Scripts/RabbitSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitSpawner : MonoBehaviour
+{
+    public Transform prefabEvilRabbitLeft;
+    public Transform prefabEvilRabbitRight;
+    public Transform prefabGoodRabbitLeft;
+    public Transform prefabGoodRabbitRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InstantiateEvilRabbit", 0, 5);
+        InvokeRepeating("InstantiateGoodRabbit", 0, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void InstantiateEvilRabbit()
+    {
+        Instantiate(prefabEvilRabbitLeft, new Vector3(prefabEvilRabbitLeft.transform.position.x, prefabEvilRabbitLeft.transform.position.y, Random.Range(-120, -70)), prefabEvilRabbitLeft.transform.rotation);
+        Instantiate(prefabEvilRabbitRight, new Vector3(prefabEvilRabbitRight.transform.position.x, prefabEvilRabbitRight.transform.position.y, Random.Range(-120, -70)), prefabEvilRabbitRight.transform.rotation);
+    }
+
+    void InstantiateGoodRabbit()
+    {
+        Instantiate(prefabGoodRabbitLeft, new Vector3(prefabGoodRabbitLeft.transform.position.x, prefabGoodRabbitLeft.transform.position.y, Random.Range(-120, -70)), prefabGoodRabbitLeft.transform.rotation);
+        Instantiate(prefabGoodRabbitRight, new Vector3(prefabGoodRabbitRight.transform.position.x, prefabGoodRabbitRight.transform.position.y, Random.Range(-120, -70)), prefabGoodRabbitRight.transform.rotation);
+    }
+}
